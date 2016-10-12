f4616cbfd50849d1dd96988e03b271909a52bc8d
liconrepo <khairul.alam.licon@framgia.com>
2016-10-12 08:43:02 +0700

    content fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "LiconFromFramgia", selector: "#liconfeed" });
+    GitHubActivity.feed({ username: "liconrepo", selector: "#liconfeed" });
 
 
 });
